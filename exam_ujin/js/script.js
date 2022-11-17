4b296bf75d1ed0859ee024fa90be181066643c1e
DESKTOP-4ETTRC2\start <start2458@gmail.com>
2022-11-17 09:51:12 +0900

    라면먹고싶다

--- exam_ujin/js/script.js
+++ exam_ujin/js/script.js
@@ -1,5 +1,14 @@
 $(function(){
 
+//slide
+    
+    let current = 0;
+    setInterval(function(){
+        let next = (current+1)%3;
 
+        $(".slideList").find("div").eq(current).fadeOut();
+        $(".slideList").find("div").eq(next).fadeIn();
+        current = next;
+    }, 3000)
 });
 
